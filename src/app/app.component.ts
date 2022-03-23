1dbad3239c7e55405f521da0029c1295ef81aed0
bnanaday <bnanaday@gmail.com>
2022-03-23 11:35:56 -0500

    practicing directives, did 6 steps from slide 16 on ppt "05 - Angular"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  isVisible: boolean = true;
+  showOrHide: boolean = true;
   count: number = 0;
-  LOG:number[] = [];
+  log: number[] = [];
 
   onClick() {
-    this.isVisible = !this.isVisible;
-    this.LOG.push(this.count++);
+    this.showOrHide = !this.showOrHide;
+    this.log.push(this.count++);
   }
 }
